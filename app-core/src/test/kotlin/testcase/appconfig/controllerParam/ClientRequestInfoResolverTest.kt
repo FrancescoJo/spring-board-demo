55b348b00c763244e655b6bd22f19e10fd6284a7
Francesco Jo <nimbusob@gmail.com>
2020-07-17 02:50:51 +0900

    Bump Controller parameter 'ClientRequestInfo' resolver

--- app-core/src/test/kotlin/testcase/appconfig/controllerParam/ClientRequestInfoResolverTest.kt
+++ app-core/src/test/kotlin/testcase/appconfig/controllerParam/ClientRequestInfoResolverTest.kt
@@ -8,11 +8,18 @@ import com.github.fj.board.appconfig.controllerParam.ClientRequestInfoResolver
 import com.github.fj.board.component.auth.AuthenticationObjectImpl
 import com.github.fj.board.exception.client.UnauthorisedException
 import com.github.fj.board.exception.server.NotImplementedException
+import com.github.fj.board.persistence.model.auth.PlatformType
 import com.github.fj.board.vo.auth.ClientRequestInfo
+import com.github.fj.board.vo.auth.UserAgent
+import com.github.fj.lib.util.getRandomAlphaNumericString
+import de.skuzzle.semantic.Version
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
 import org.springframework.core.MethodParameter
 import org.springframework.security.core.Authentication
@@ -20,6 +27,8 @@ import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.context.request.NativeWebRequest
 import test.com.github.fj.board.component.auth.JwtObjectBuilder
 import test.com.github.fj.board.component.security.HttpAuthorizationTokenBuilder
+import test.com.github.fj.board.util.HttpRequestUtils.mockLocalhostServletRequest
+import test.com.github.fj.lib.util.RandomTestArgUtils.randomEnumConst
 
 /**
  * @author Francesco Jo(nimbusob@gmail.com)
@@ -76,7 +85,34 @@ class ClientRequestInfoResolverTest {
         }
     }
 
-    // success
+    @Test
+    fun `success if all conditions are good`() {
+        // given:
+        val webReq = mock(NativeWebRequest::class.java)
+        val authObject = AuthenticationObjectImpl(
+            JwtObjectBuilder.createRandom(), HttpAuthorizationTokenBuilder.createRandom()
+        )
+        val mockHttpReq = mockLocalhostServletRequest()
+        val mockUa = UserAgent.create(
+            randomEnumConst(PlatformType::class.java), getRandomAlphaNumericString(16), Version.COMPLIANCE
+        )
+
+        // when:
+        SecurityContextHolder.getContext().authentication = authObject
+        `when`(mockHttpReq.getHeader(UserAgent.HEADER_NAME)).thenReturn(
+            "${mockUa.platformType.userAgentName}; ${mockUa.appVer}; ${mockUa.platformVer}"
+        )
+        `when`(webReq.nativeRequest).thenReturn(mockHttpReq)
+
+        // then:
+        val result = testResolveArgument(webReq)
+
+        // expect:
+        assertThat(result.loginName, `is`(authObject.name))
+        assertThat(result.platformType, `is`(mockUa.platformType))
+        assertThat(result.platformVer, `is`(mockUa.platformVer))
+        assertThat(result.appVer, `is`(mockUa.appVer))
+    }
 
     @AfterEach
     fun teardown() {
