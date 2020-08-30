e9d889a17ef58f4439a41ee0bf4c82634080dea7
Francesco Jo <nimbusob@gmail.com>
2020-08-30 13:59:40 +0900

    #48: Integrate flutter/web project into build phase

---
+++ app-main/build_flutter_frontend.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+SET "CURRENT_DIR=%cd%"
+
+PUSHD %CURRENT_DIR%
+
+CD src\frontend
+
+cmd /c flutter build web
+
+IF /I "%errorlevel%" NEQ "0" (
+    POPD
+    ECHO ERROR: Please follow instructions below to troubleshoot if:
+    ECHO   - 'flutter' command is not found, read https://flutter.dev/docs/get-started/install/windows
+    ECHO   - 'flutter build web' is failed , read https://flutter.dev/docs/get-started/web
+    EXIT /B 1
+)
+
+xcopy /E build\web\* %CURRENT_DIR%\src\main\resources\static
+
+POPD
---
+++ app-main/build_flutter_frontend.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+CURRENT_DIR=`pwd`
+
+pushd $CURRENT_DIR% > /dev/null
+
+cd src/frontend
+
+flutter build web
+
+if [ "$?" -eq "1" ]; then
+    popd
+    echo ERROR: Please follow instructions below to troubleshoot if:
+    echo   - 'flutter' command is not found, read https://flutter.dev/docs/get-started/install/windows
+    echo   - 'flutter build web' is failed , read https://flutter.dev/docs/get-started/web
+    exit 1
+fi
+
+cp -r build/web/* $CURRENT_DIR/src/main/resources/static/
+
+popd
